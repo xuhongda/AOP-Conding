6915223151d38536d3965d6f1142f80a30e1ca94
xuhongda <xuhongda7@outlook.com>
2018-11-21 15:43:53 +0800

    说明

--- src/main/java/com/xu/proxy/动态/ObjInvocationHandler.java
+++ src/main/java/com/xu/proxy/动态/ObjInvocationHandler.java
@@ -21,7 +21,10 @@ public class ObjInvocationHandler<T> implements InvocationHandler {
     }
 
     /**
-     *
+     *<p>
+     * 将横切代码与业务代码编织，可以在业务代码上下任意的添加横切逻辑代码
+     * 这就是动态代理的优点。。。。。。
+     *</p>
      * @param proxy 动态代理对象
      * @param method 正在执行的方法
      * @param args 调用目标方法时传入的实参
@@ -30,9 +33,11 @@ public class ObjInvocationHandler<T> implements InvocationHandler {
      */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
+        //横切逻辑代码1
         MyFunction.start();
+        //业务逻辑代码
         Object result = method.invoke(target, args);
+        // 横切逻辑代码2
         MyFunction.end();
         return result;
 
